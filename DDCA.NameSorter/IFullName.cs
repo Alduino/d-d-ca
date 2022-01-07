87652693d71c3d98fdf43aaa715b96acab2c973f
Alduino <git@alduino.dev>
2022-01-07 19:24:04 +1000

    Remove length attributes

--- DDCA.NameSorter/IFullName.cs
+++ DDCA.NameSorter/IFullName.cs
@@ -5,9 +5,7 @@ namespace DDCA.NameSorter
     public interface IFullName
     {
         string LastName { get; }
-        
-        [MinLength(1)]
-        [MaxLength(3)]
+
         string[] GivenNames { get; }
     }
 }
