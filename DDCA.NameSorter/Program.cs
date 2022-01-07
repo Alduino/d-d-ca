a23fa3ea1310884e0002de2384f0f89410cbc86a
Alduino <git@alduino.dev>
2022-01-07 10:46:19 +1000

    Initial commit

---
+++ DDCA.NameSorter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DDCA.NameSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
