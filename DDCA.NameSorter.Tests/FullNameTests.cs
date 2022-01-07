83ca1ded10af30caca789172913adfacd970eb7d
Alduino <git@alduino.dev>
2022-01-07 14:29:30 +1000

    Add test for first/last name constructor

--- DDCA.NameSorter.Tests/FullNameTests.cs
+++ DDCA.NameSorter.Tests/FullNameTests.cs
@@ -33,5 +33,15 @@ namespace DDCA.NameSorter.Tests
             var exception = Assert.Throws<ArgumentException>(() => FullName.Parse(source));
             Assert.AreEqual(exceptionMessage, exception.Message);
         }
+
+        [Test]
+        public void FirstLastNameConstructor()
+        {
+            var actual = new FullName("Parsons", "Janet");
+            var expected = new FullName("Parsons", new[] { "Janet" });
+            
+            Assert.AreEqual(expected.LastName, actual.LastName);
+            Assert.IsTrue(expected.GivenNames.SequenceEqual(actual.GivenNames));
+        }
     }
 }
