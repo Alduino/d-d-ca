cec11bbf53ae50d8aa93ed7b57b145e9f8186d3c
Alduino <git@alduino.dev>
2022-01-07 13:55:58 +1000

    Create tests for full name parser

--- DDCA.NameSorter/FullName.cs
+++ DDCA.NameSorter/FullName.cs
@@ -5,6 +5,18 @@ namespace DDCA.NameSorter
 {
     public class FullName : IFullName
     {
+        /// <summary>
+        /// Parses a FullName where each part of the name is separated by spaces. A FullName has a last name, and
+        /// between one and three given names.
+        /// </summary>
+        /// <param name="source">The source text to parse the full name. Should be trimmed and have a single space
+        /// between parts.</param>
+        /// <returns>A full name parsed from the string</returns>
+        public static FullName Parse(string source)
+        {
+            throw new NotImplementedException();
+        }
+
         public FullName(string lastName, string firstName) : this(lastName, new[] { firstName })
         {
         }
