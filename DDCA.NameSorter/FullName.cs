d336b25d51afc30f0c01a274876e8a191c5da948
Alduino <git@alduino.dev>
2022-01-07 14:21:28 +1000

    Implement FullName parse method

--- DDCA.NameSorter/FullName.cs
+++ DDCA.NameSorter/FullName.cs
@@ -14,7 +14,43 @@ namespace DDCA.NameSorter
         /// <returns>A full name parsed from the string</returns>
         public static FullName Parse(string source)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(source))
+            {
+                throw new ArgumentException("Full name cannot be empty or whitespace");
+            }
+
+            if (source.StartsWith(" "))
+            {
+                throw new ArgumentException("Spaces not allowed at beginning of full name");
+            }
+
+            if (source.EndsWith(" "))
+            {
+                throw new ArgumentException("Spaces not allowed at end of full name");
+            }
+
+            var parts = source.Split(' ');
+
+            if (parts.Length < 2)
+            {
+                throw new ArgumentException("Not enough parts to construct full name");
+            }
+
+            if (parts.Any(string.IsNullOrWhiteSpace))
+            {
+                throw new ArgumentException("Must not have extra spaces between parts");
+            }
+
+            var givenNames = parts.Take(parts.Length - 1).ToArray();
+
+            if (givenNames.Length > 3)
+            {
+                throw new ArgumentException("Too many given names");
+            }
+
+            var lastName = parts.Last();
+
+            return new FullName(lastName, givenNames);
         }
 
         public FullName(string lastName, string firstName) : this(lastName, new[] { firstName })
